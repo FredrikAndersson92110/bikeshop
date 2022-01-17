cd824e261c18e71236fe2673841de574ea09519a
fredrik Andersson <fandersson1121@gmail.com>
2022-01-17 17:00:32 +0100

    Add carousel

--- routes/index.js
+++ routes/index.js
@@ -56,12 +56,12 @@ router.post('/create-checkout-session', async (req, res) => {
  // end stripe
  
 var dataBike = [
-  {name:"BIK045", url:"/images/ruff.jpg", price:679},
-  {name:"ZOOK07", url:"/images/velo-electrique.jpg", price:999},
-  {name:"TITANS", url:"/images/velobecan.jpg", price:799},
-  {name:"CEWO", url:"/images/fixie.jpg", price:1300},
-  {name:"AMIG039", url:"/images/peugeot.jpg", price:479},
-  {name:"LIK099", url:"/images/brompton.jpg", price:869},
+  {mea: true, name:"BIK045", url:"/images/ruff.jpg", price:679},
+  {mea: false, name:"ZOOK07", url:"/images/velo-electrique.jpg", price:999},
+  {mea: true, name:"TITANS", url:"/images/velobecan.jpg", price:799},
+  {mea: false, name:"CEWO", url:"/images/fixie.jpg", price:1300},
+  {mea: true, name:"AMIG039", url:"/images/peugeot.jpg", price:479},
+  {mea: false, name:"LIK099", url:"/images/brompton.jpg", price:869},
 ]
 
 /* GET home page. */
